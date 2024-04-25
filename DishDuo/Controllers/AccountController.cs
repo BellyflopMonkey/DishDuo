8690f8276cb0f79da7468bed3f73a8016b94a53f
Ben Cupery <bcupery2@student.morainepark.edu>
2024-04-25 09:51:41 -0500

    BC - commented controllers 04/25/2024

--- DishDuo/Controllers/AccountController.cs
+++ DishDuo/Controllers/AccountController.cs
@@ -16,65 +16,65 @@ namespace DishDuo.Controllers
             _signInManager = signInManager;
         }
 
-        public IActionResult Login()
+        public IActionResult Login() //Login actionresult method
         {
-            return View();
+            return View(); //Returns the "Login" view
         }
         [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel login, string? returnUrl)
+        public async Task<IActionResult> Login(LoginViewModel login, string? returnUrl) //Login task method
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid) //Checks if the ModelState is valid
             {
-                var result = await _signInManager.PasswordSignInAsync(login.Email, login.Password, false, false);
-                if (result.Succeeded)
+                var result = await _signInManager.PasswordSignInAsync(login.Email, login.Password, false, false); //Variable for sign-in using _signinmanager
+                if (result.Succeeded) //Checks if result variable was successful
                 {
-                    if(!string.IsNullOrEmpty(returnUrl))
-                        return LocalRedirect(returnUrl);
-                    return RedirectToAction("Index", "Home");
+                    if(!string.IsNullOrEmpty(returnUrl)) //Checks if the return url has a value
+                        return LocalRedirect(returnUrl); //Redirects to the return url if it has a value
+                    return RedirectToAction("Index", "Home"); //Redirects to the homepage if it has no value
                 }
 
-                ModelState.AddModelError("", "Invalid login Attempt");
+                ModelState.AddModelError("", "Invalid login Attempt"); //Displays if the result variable fails
             }
-            return View(login);
+            return View(login); //Redirects to login screen
         }
 
-        public async Task<IActionResult> Logout()
+        public async Task<IActionResult> Logout() //Logout task method
         {
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
+            await _signInManager.SignOutAsync(); //Log out await using _signinmanager
+            return RedirectToAction("Index", "Home"); //Redirects to homepage
         }
 
 
-        public IActionResult Register()
+        public IActionResult Register() //Register result method
         {
-            return View();
+            return View(); //Returns register view
         }
         [HttpPost]
-        public async Task<IActionResult> Register(RegisterViewModel register)
+        public async Task<IActionResult> Register(RegisterViewModel register) //Register task method
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid) //Checks if the modelstate is valid
             {
-                var user = new ApplicationUser()
+                var user = new ApplicationUser() //user variable from ApplicationUser class
                 {
-                    Name = register.Name,                    
-                    Email = register.Email,
-                    UserName = register.Email
+                    Name = register.Name, //Name is set to inputted name
+                    Email = register.Email, //Email is set to inputted email
+                    UserName = register.Email //UserName is set to inputted email
                 };
-                var result = await _userManager.CreateAsync(user, register.Password);
-                if (result.Succeeded)
+                var result = await _userManager.CreateAsync(user, register.Password); //Result variable for creating a user
+                if (result.Succeeded) //Checks id the result variable succeeded
                 {
-                    await _signInManager.PasswordSignInAsync(user, register.Password, false, false);
-                    return RedirectToAction("Login", "Account");
+                    await _signInManager.PasswordSignInAsync(user, register.Password, false, false); //Sign in await
+                    return RedirectToAction("Login", "Account"); //Redirects to login screen
                 }
-                else
+                else //Occurs if the id result fails
                 {
-                    foreach (var err in result.Errors)
+                    foreach (var err in result.Errors) //loops for each error in the result variable
                     {
-                        ModelState.AddModelError("", err.Description);
+                        ModelState.AddModelError("", err.Description); //Displays an error message for each error
                     }
                 }
             }
-            return View(register);
+            return View(register); //Returns the register view
         }
     }
 }
--- DishDuo/Controllers/CartController.cs
+++ DishDuo/Controllers/CartController.cs
@@ -12,63 +12,63 @@ namespace DishDuo.Controllers
         private readonly IData data;
         private readonly DishDuoDBContext context;
 
-        public CartController(IData data, DishDuoDBContext context)
+        public CartController(IData data, DishDuoDBContext context) //CartController constructor
         {
-            this.data = data;
-            this.context = context;
+            this.data = data; //Sets data to IData varaible
+            this.context = context; //Sets context to DishDuoDBContext variable
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index() //Index task method
         {
-            var user = await data.GetUser(HttpContext.User);
-            var cartsList = context.Carts.Where(c => c.UserId == user.Id).ToList();
-            return View(cartsList);
+            var user = await data.GetUser(HttpContext.User); //user Variable is set to HTTPContext user
+            var cartsList = context.Carts.Where(c => c.UserId == user.Id).ToList(); //Cartslist variable is set to carts context as a list
+            return View(cartsList); //Returns cart view with the cartsList variable
         }
 
         [HttpPost]
-        public async Task<IActionResult> SaveCart(Cart cart)
+        public async Task<IActionResult> SaveCart(Cart cart) //SaveCart task method
         {
-            var user = await data.GetUser(HttpContext.User);
-            cart.UserId = user?.Id;
+            var user = await data.GetUser(HttpContext.User); //user Variable is set to HTTPContext user
+            cart.UserId = user?.Id; //UserId in cart is set to the userId
 
-            if (ModelState.IsValid)
+            if (ModelState.IsValid) //Checks if the modelstate is valid
             {
-                context.Carts.Add(cart);
-                context.SaveChanges();
-                return Ok(cart);
+                context.Carts.Add(cart); //adds the cart to the cart context
+                context.SaveChanges(); //Saves changes to context
+                return Ok(cart); //returns cart
             }
 
-            return BadRequest();
+            return BadRequest(); //returns BadRequest
         }
         [HttpGet]
-        public async Task<IActionResult> GetAddedCarts()
+        public async Task<IActionResult> GetAddedCarts() //GetAddedCarts method
         {
-            var user = await data.GetUser(HttpContext.User);
-            var carts = context.Carts.Where(c => c.UserId == user.Id).Select(c => c.RecipeId).ToList();
+            var user = await data.GetUser(HttpContext.User); //user Variable is set to HTTPContext user
+            var carts = context.Carts.Where(c => c.UserId == user.Id).Select(c => c.RecipeId).ToList(); //Carts variable is set to carts context as a list
             return Ok(carts);
         }
         [HttpPost]
-        public IActionResult RemoveCartFromList(string Id)
+        public IActionResult RemoveCartFromList(string Id) //RemoveCartFromList action result method
         {
-            if (!string.IsNullOrEmpty(Id))
+            if (!string.IsNullOrEmpty(Id)) //Runs if the Id string is not null/empty
             {
-                var cart = context.Carts.Where(c => c.RecipeId == Id).FirstOrDefault();
-                if (cart != null)
+                var cart = context.Carts.Where(c => c.RecipeId == Id).FirstOrDefault(); //Sets cart variable to carts context using current Id
+                if (cart != null) //Runs if the cart is not null
                 {
-                    context.Carts.Remove(cart);
-                    context.SaveChanges();
+                    context.Carts.Remove(cart); //Removes current cart item
+                    context.SaveChanges(); //Saves context changes
                     return Ok();
                 }
             }
-            return BadRequest();
+            return BadRequest(); //Returns BadRequest
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetCartList()
+        public async Task<IActionResult> GetCartList() //GetCartList task method
         {
-            var user = await data.GetUser(HttpContext.User);
-            var cartList = context.Carts.Where(c => c.UserId == user.Id).Take(3).ToList();
-            return PartialView("_CartList", cartList);
+            var user = await data.GetUser(HttpContext.User); //user Variable is set to HTTPContext user
+            var cartList = context.Carts.Where(c => c.UserId == user.Id).Take(3).ToList(); //CartsList variable is set to carts context as a list
+            return PartialView("_CartList", cartList); //Returns a partialview of the cartlist
         }
     }
 }
